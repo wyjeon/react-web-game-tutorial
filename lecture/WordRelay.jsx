aef662cb6125eb154533038a3f2586a699dbfaa3
wyjeon <61363233+wyjeon@users.noreply.github.com>
2021-01-11 13:24:06 +0900

    Update WordRelay game

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -3,11 +3,50 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "hello",
+    word: "테스트",
+    value: "",
+    result: "",
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if(this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: "정답 ",
+        word: this.state.value,
+        value: "",
+      });
+    } else {
+      this.setState({
+        result: "땡 ",
+        value: "",
+      });
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({value: e.target.value});
+  };
+
+  input;
+  onRefInput = (c) => {
+    this.input = c;
   };
 
   render() {
-    return <h1>{this.state.text}</h1>
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input 
+            ref={this.onRefInput} 
+            onChange={this.onChangeInput} 
+            value={this.state.value}/>
+          <button>입력</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
