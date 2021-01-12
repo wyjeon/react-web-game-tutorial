eba258035de27a68f90c10a46dd386c2f5c6c8b3
wyjeon <61363233+wyjeon@users.noreply.github.com>
2021-01-12 13:23:55 +0900

    Update Response check game

---
+++ 반응속도체크/ResponseCheck.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+
+class ResponseCheck extends Component {
+  state = {
+    state: "waiting",
+    message: "클릭해서 시작하세요",
+    result: [],
+  };
+
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === "waiting") {
+      this.setState({
+        state: "ready",
+        message: "초록색이 되면 클릭하세요",
+      });
+
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: "now",
+          message: "지금 클릭!!!!",
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000 + 2000)); // 2~3초 랜덤
+    } else if (state === "ready") {
+      // 성급하게 클릭
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "성급하셨네요 초록색이 된 후 클릭하세요",
+      });
+    } else if (state === "now") {
+      // 반응속도 체크
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: "waiting",
+          message: "클릭해서 시작하세요",
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
+    }
+  };
+
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return result.length === 0 ? null : (
+      <>
+        <div>
+          평균 시간:{" "}
+          {this.state.result.reduce((a, c) => a + c) / this.state.result.length}{" "}
+          ms
+        </div>
+        <button onClick={this.onReset}>리셋</button>
+      </>
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <div
+          id="screen"
+          className={this.state.state}
+          onClick={this.onClickScreen}
+        >
+          {this.state.message};
+        </div>
+        {this.renderAverage()}
+      </>
+    );
+  }
+}
+
+export default ResponseCheck;
